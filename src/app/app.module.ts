240187f244a6c53a96011d6e671e85353df27980
Emm4nuell <eduardo.eesf@gmail.com>
2024-05-30 14:14:33 -0300

    Pagina criar orçamento adicionado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CriarorcamentoComponent } from './components/pages/orcamento/criarorcamento/criarorcamento.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CriarorcamentoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/pages/orcamento/criarorcamento/criarorcamento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criarorcamento',
+  templateUrl: './criarorcamento.component.html',
+  styleUrls: ['./criarorcamento.component.css']
+})
+export class CriarorcamentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
